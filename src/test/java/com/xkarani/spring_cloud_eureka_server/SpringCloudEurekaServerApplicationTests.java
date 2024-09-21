1e67dbc0f6e9b7a9f5bcad725590a3113232095a
xkarani <xkarani.devops@gmail.com>
2024-09-21 16:06:33 +0530

    Eureka discovery server code base

---
+++ src/test/java/com/xkarani/spring_cloud_eureka_server/SpringCloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xkarani.spring_cloud_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
